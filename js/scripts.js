b68eef65c8daa62553cc3da28c689944ccc2ac49
ExoPlant <lennyj2468+github@gmail.com>
2021-09-01 13:23:09 +0100

    Add showing/hiding Storage Recursion + NoMoreScore

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,61 @@ function showPage() {
   document.getElementById("loading").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+/* --- Silk That Amethyst --- */
+
+function showSilkAmethyst() 
+{
+    document.getElementById("silkthatamethyst").style.display = "block";
+    document.getElementById("silkthatamethyst").onclick = "hideAmethyst()";
+}
+
+/* --- Storage Recursion --- */
+
 function showStorageRecursion() 
 {
     document.getElementById("storagerecursion").style.display = "block";
+    document.getElementById("storagerecursion").setAttribute("onclick","hideStorageRecursion()");
 }
+
+function hideStorage() {
+  document.getElementById("hideStorage").style.textDecoration = "underline";
+  document.getElementById("showStorage").style.textDecoration = "none";
+  hideStorageRecursion();
+}
+
+function showStorage() {
+  document.getElementById("showStorage").style.textDecoration = "underline";
+  document.getElementById("hideStorage").style.textDecoration = "none";
+  showStorageRecursion();
+}
+
+function hideStorageRecursion() 
+{
+  document.getElementById("storagerecursion").style.display = "none";
+}
+
+/* --- NoMoreScore --- */
+
 function showNoMoreScore() 
 {
     document.getElementById("rmscore").style.display = "block";
 }
-function showAmethyst() 
-{
-    document.getElementById("silkthatamethyst").style.display = "block";
+
+function hideScore() {
+  document.getElementById("hideScore").style.textDecoration = "underline";
+  document.getElementById("showScore").style.textDecoration = "none";
+  hideNoMoreScore();
 }
+
+function showScore() {
+  document.getElementById("showScore").style.textDecoration = "underline";
+  document.getElementById("hideScore").style.textDecoration = "none";
+  showNoMoreScore();
+}
+
+function hideNoMoreScore() 
+{
+  document.getElementById("rmscore").style.display = "none";
+}
+
